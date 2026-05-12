080adf401ec7f2e20665a3f72449d2fc88c2824a
sharathg97 <42089276+sharathg97@users.noreply.github.com>
2026-05-12 16:23:08 +0530

    Create backend.tf

---
+++ aks_acr_infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+   // resource_group_name  = "himalaya-backendrg"
+   // storage_account_name = "himalayastorage"
+   // container_name       = "tfstate"
+   // key                  = "aks.terraform.tfstate"
+  }
+}
