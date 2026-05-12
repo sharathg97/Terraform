84e1ad8bccdbdebd0570e02d42d366f0e7e65270
sharathg97 <42089276+sharathg97@users.noreply.github.com>
2026-05-12 16:21:52 +0530

    Create main.tf

---
+++ aks_acr_infra/main.tf
@@ -0,0 +1,61 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "aks-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "akscluster"
+
+  kubernetes_version = null
+
+  default_node_pool {
+    name           = "nodepool"
+    node_count     = var.node_count
+    vm_size        = var.vm_size
+    vnet_subnet_id = azurerm_subnet.subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+  network_plugin = "azure"
+  service_cidr   = "10.2.0.0/16"
+  dns_service_ip = "10.2.0.10"
+}
+
+  role_based_access_control_enabled = true
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
